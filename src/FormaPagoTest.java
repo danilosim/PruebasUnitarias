32ac6b8f9e2b717c2944104c06dc62cef6297a3b
Wenceslao <villegaswences@gmail.com>
2019-08-20 20:35:06 -0300

    agregado primer test vacio

---
+++ src/FormaPagoTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Wences on 20/8/2019.
+ */
+public class FormaPagoTest {
+    @org.junit.Test
+    public void getId() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setId() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getName() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setName() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getInteres() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setInteres() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getPromoDia() throws Exception {
+
+    }
+
+}
